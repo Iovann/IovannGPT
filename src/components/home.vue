d0e05969d57159fdbf5f6fb7b2bb879bdcf66dd0
iovann <iovannatcho@gmail.com>
2024-04-18 17:28:06 +0100

    update

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,23 @@
 <template>
   <section class="container-fluid">
     <div class="offcanvas offcanvas-start black d-lg-none" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <p class="text-end mb-0">
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </p>
       <div class="offcanvas-header">
-        <div class="container open px-1 py-2 mt-4 rounded-pill">
-              <div class="col-1 me-3 ">
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-              </div>
-            </div>
+        <div class="container open px-1 py-2 mt-lg-4 rounded-pill">
+              <div class="col-9 me-3 ">
+                <div class="row justify-content-between align-items-center">
+                  <div class="row justify-content-between align-items-center">
+                    <div class="col-9">
+                      <img src="/icons/mini.svg" alt="" class="bg-white p-1 rounded-circle">
+                      <span class="ms-1 text-white fw-bolder">New chat</span>
+                    </div>
+                  </div>
+                </div>
         </div>
 
-        <div class="row justify-content-between align-items-center">
-              <div class="row justify-content-between align-items-center">
-              <div class="col-9">
-                <img src="/icons/mini.svg" alt="" class="bg-white p-1 rounded-circle">
-                <span class="ms-1 text-white fw-bolder">New chat</span>
-        </div>
+        
 
       </div>
       </div>
@@ -72,49 +75,56 @@
 
       <div class="col-lg-9 col-xl-10 bg-violet container vh-100 ">
         <!-- menu -->
-          <div class="row mt-4 justify-content-between">
+          <div class="row mt-4 justify-content-between d-none d-lg-flex">
             <div class="col-6 d-flex justify-content-between">
-              <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><img src="/icons/hmg.svg" alt=""></button>
               <div class="dropdown">
-                <a class="btn text-white dropdown-toggle fw-bolder " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Chat GPT 3.5
-                </a>
-                <!-- <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul> -->
+                <a class="btn text-white dropdown-toggle fw-bolder " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chat GPT 3.5</a>
               </div>
             </div>
 
-            <div class="col-3 d-flex justify-content-end ">
-                <img src="/icons/export.svg" alt="" class=" me-3" style="width: 6%;">
+            <div class="col-3 d-none d-lg-flex justify-content-end ">
+                <p class="text-end">
+                  <img src="/icons/export.svg" alt="" class="me-3">
+                </p>
+            </div>
+          </div>
+
+          <div class="row mt-4 justify-content-between d-lg-none">
+            <div class="col d-flex justify-content-between">
+              <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><img src="/icons/hmg.svg" alt=""></button>
+              <div class="dropdown">
+                <a class="btn text-white dropdown-toggle fw-bolder " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Chat GPT 3.5</a>
+              </div>
+
+              <img src="/icons/write.svg" alt="" class=" me-3">
             </div>
           </div>
 
           <!-- middle -->
-          <div class="row  align-items-center " style="min-height: 45% !important;">
-              <div class="text-center pb-3 pt-5">
-                <img src="/icons/chat.svg" alt="" class=" bg-white p-3 rounded-circle">
-                <p class="text-center fw-bold text-white fs-4 mt-3 ">How can I help you today?</p>
-              </div>
-          </div>
 
-          <div class="row">
-            <div class="container px-5 text-white">
-                  <div class="row justify-content-center gap-3">
-                    <div class="col-lg-5 col-md-11 col-11 rounded-4 px-2 px-lg-4 py-2 border border-1 whitebord open2">
+          <div class="row justify-content-center align-items-center h-50">
+            <div class="col-8">
+                <p class="text-center"><img src="/icons/chat.svg" alt="" class=" bg-white p-3 rounded-circle"></p>
+                <p class="text-center fw-bold text-white fs-4 mt-3 ">How can I help you today?</p>
+            </div>
+          </div>
+                
+          <div class="row justify-content-center align-content-center pt-5">
+            <div class="col-lg-9 col-xl-10 col-12  mt-auto">
+              <div class="container text-white">
+                  <div class="row justify-content-center gap-2 gap-lg-3">
+                    <div class="col-lg-5 col-11 rounded-4 px-2 px-lg-2 px-xl-4  py-2 border border-1 whitebord open2">
                       <p class="fw-bold fs2 mb-2">Brainstorm contest ideas</p>
                       <p class="fs1">for my new podcast on urban design</p>
                     </div>
 
-                    <div class="col-lg-5 col-md-11 col-11 rounded-4 px-2 px-lg-4 py-2 border border-1 whitebord open2">
+                    <div class="col-lg-5 col-11 rounded-4 px-2 px-lg-2 px-xl-4  py-2 border border-1 whitebord open2">
                       <p class="fw-bold fs2 mb-2">Design a database schema</p>
                       <p class="fs1">for an online merch store</p>
                     </div>
                   </div>
 
-                  <div class="row justify-content-center d-none d-xl-flex gap-3 mt-4">
+                  <div class="row justify-content-center d-none d-xl-flex gap-2 gap-lg-3 mt-4">
                     <div class="col-lg-5 rounded-4 px-4 py-2 border border-1 whitebord open2">
                       <p class="fw-bold fs2 mb-2">Come up with concepts</p>
                       <p class="fs1">for a retro-style arcade game</p>
@@ -124,63 +134,38 @@
                       <p class="fw-bold  fs2 mb-2">Help me oick</p>
                       <p class="fs1">an outfit that will look good on camera</p>
                     </div>
+
+
+                    <div class="row justify-content-center ">
+                    <div class="col-12 col-sm-11 mx-2 px-sm-1 ">
+                      <form action="">
+                      <div class="input-group mt-3 mb-2">
+                        <input class="form-control form-control-lg rounded-start-4" v-model="message" type="text" placeholder="Message ChatGPT..." style="background-color: #212121; color:white; border-right-color: #212121 !important;" id="message" aria-describedby="basic-addon2">
+                        <button class="input-group-text rounded-end-4 bg-violet bord p-3" style="border-left-color:#212121 !important;"  type="submit" id="basic-addon2"><img src="/icons/arrow.svg" :style="{backgroundColor: message? 'white': '#A2A2A8'}" alt="" class="p-2 rounded-2"></button>
+                      </div>
+                    </form>
+                    </div>
                   </div>
-              </div>
-          </div>
-
-          <!-- end -->
-        <div class="d-flex container justify-content-center align-items-end " style="min-height: 20%;">
-            <div class="row align-items-end justify-content-center">
-              <div class=" col-10 col-md-12 align-self-end">
-                <form action="">
-                  <div class="input-group mt-3 mb-2">
-                    <input class="form-control form-control-lg rounded-start-4" v-model="search" type="text" placeholder="Message ChatGPT..." style="background-color: #212121; color:white; border-right-color: #212121 !important;" id="message" aria-describedby="basic-addon2">
-                    <button class="input-group-text rounded-end-4 bg-violet bord p-3" style="border-left-color:#212121 !important;" :class="{'disabled' : activate === false}" type="submit" id="basic-addon2"><img src="/icons/arrow.svg" alt="" class="p-2 rounded-2 bg-white"></button>
                   </div>
-                </form>
+                  <p class="text-center text-white position-relative">
+                    <span class="ps-lg-5 ms-lg-5 ">ChatGPT can make mistakes. Consider checking important information.</span>
+                  </p>
               </div>
-
-
-            <div class="row align-items-center">
-            <div class="col-11 align-content-center">
-              <p class="text-center text-white position-relative">
-                <span class="ps-lg-5 ms-lg-5 ">ChatGPT can make mistakes. Consider checking important information.</span>
-              </p>
-            </div>
-
-            <div class="col-1 d-none d-block text-end">
-              <span class="py-2 px-3 bg-violet border border-1 whitebord rounded-circle fs-4 fw-bolder text-white" style="background-color: #474444;">?</span>
-            </div>
-          </div>
           </div>
         </div>
-      </div>
+      </div> 
     </div>
   </section>
 </template>
 
 <script>
-import { watch, ref } from 'vue'
 export default {
-  setup() {
-    let search = ref('');
-    let activate = false; // Initialize to false by default
-    console.log(activate)
-    watch(search, (new_value) => {
-      console.log(new_value);
-      if (new_value.length > 0) { // Check for length greater than 0 (not empty)
-        console.log(activate)
-        activate = true;
-      } else {
-        activate = false;
-      }
-    })
-
-    return {
-      search,
-      activate,
-    };
-  },
+  data(){
+    return{
+      message: "",
+    }
+    
+  }
 }
 </script>
 
